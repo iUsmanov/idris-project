55de49f54130755db68c2f0ad9b577de8e206272
GoodProgerman <my-git-hub@mail.ru>
2023-08-09 13:19:42 +0300

    buildSlice, buildSelector

---
+++ src/shared/lib/store/buildSlice.ts
@@ -0,0 +1,28 @@
+import {
+	CreateSliceOptions,
+	SliceCaseReducers,
+	bindActionCreators,
+	createSlice,
+} from '@reduxjs/toolkit';
+import { useAppDispatch } from '../hooks/useAppDispatch/useAppDispatch';
+import { useMemo } from 'react';
+
+export function buildSlice<
+	State,
+	CaseReducers extends SliceCaseReducers<State>,
+	Name extends string = string
+>(options: CreateSliceOptions<State, CaseReducers, Name>) {
+	const slice = createSlice(options);
+
+	const useActions = (): typeof slice.actions => {
+		const dispatch = useAppDispatch();
+
+		// @ts-ignore
+		return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch]);
+	};
+
+	return {
+		...slice,
+		useActions,
+	};
+}
