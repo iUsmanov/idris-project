2a41c22ba646a950022859f4b5000c56af19e3f3
Idris <my-git-hub@mail.ru>
2024-01-29 09:08:07 +0300

    Cleaning

--- cypress/support/commands/rating.ts
+++ cypress/support/commands/rating.ts
@@ -1,12 +1,7 @@
 export const setRate = (rating: number = 5, feedback: string = 'feedback') => {
 	cy.getByTestId('StarRating.5').click();
-	// cy.getByTestId('FeedbackInput').click()
 	cy.getByTestId('FeedbackInput').type(feedback);
 	cy.getByTestId('RatingCard.SendButton').click();
-	// cy.getByTestId('AddNewComment').within(() => {
-	// 	cy.get('input').type(commentText);
-	// 	cy.get('button').click();
-	// });
 };
 
 declare global {
